1143320581ea33f645e70a9e02e856d3e73a0571
GiovanaZanirato <84473403+GiovanaZanirato@users.noreply.github.com>
2021-05-26 17:00:47 -0300

    Add files via upload

---
+++ chapter_3.py
@@ -0,0 +1,24 @@
+bicycles=["milk","brigadeiro","meat"]
+print(bicycles)
+print(bicycles[2].title())
+motor=["prada","gucci","chanel","louis viton"]
+print(motor)
+motor[3]="renner"
+print(motor)
+motor.append("louis viton")
+print(motor)
+motor.insert(2,"zara")
+print(motor)
+del motor[5]
+print(motor)
+sander= motor.pop()
+print(motor)
+print(sander)
+fernanda= motor.pop(2)
+print(motor)
+car=["neils","hair","make up","blow aut"]
+print(car)
+car.remove("neils")
+print(car)
+car.sort()
+print(car)
